e725918cda2547b49501a5fe5a85416d4782a3b3
Student-ProgrammerSushan <adhikarisusan38@gmail.com>
2025-05-08 12:05:54 +0545

    Setw Manipulator

--- main.cpp
+++ main.cpp
@@ -64,3 +64,16 @@ int main(){
              << "Lowville " << pop3 << endl;
              return 0;
     }
+/*
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main(){
+long pop1 = 2425785, pop2 = 47, pop3 = 9761;
+cout << setw(8) << "Location" << setw(12) << "Population" << endl
+     << setw(8) << "Portcity" << setw(12) << pop1 << endl
+     << setw(8) << "Hightown" << setw(12) << pop2 << endl
+     << setw(8) << "Lowville" << setw(12) << pop3 << endl;
+return 0;
+    }
+    */
