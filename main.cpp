87b672626811a9327c549d05fc6b7b294a5db15e
Student-ProgrammerSushan <adhikarisusan38@gmail.com>
2025-05-08 09:58:13 +0545

    Defining integer variables
    exe file executes with the cpp file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
-#include<iostream>
+/*#include<iostream>
 int main(){
    std:: cout << "Every age has a language of it's own\n";
     return 0;
+}
+    */
+   #include <iostream>
+   using namespace std;
+   int main(){
+int var1;
+int var2;
+var1 = 20;
+var2 = var1 + 10;
+cout << var2 <<endl;
+return 0;
 }
